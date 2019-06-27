110046238da744dc6ea1af74d7982966abddfcda
AzrulZ <49754767+palohotox@users.noreply.github.com>
2019-06-27 09:59:54 +0800

    Update WebScapping.py

--- WebScapping.py
+++ WebScapping.py
@@ -1,7 +1,8 @@
+
 from bs4 import BeautifulSoup
 import requests
 import random
-
+import csv
 
 def get_imd_movies(url):
     page = requests.get(url)
@@ -25,16 +26,34 @@ def get_imd_movie_info(movie):
 
 
 def imd_movie_picker():
+
+ #newline='' to remove extract blank column
+ with open('TopMovie.csv', mode='w' ,newline='') as myfile:
+
+    my_write = csv.writer(myfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    #print("--------------------------------------------")
     ctr = 0
-    print("--------------------------------------------")
     for movie in get_imd_movies('http://www.imdb.com/chart/top'):
         movie_title, movie_year, movie_url = get_imd_movie_info(movie)
         movie_summary = get_imd_summary(movie_url)
-        print(movie_title, movie_year)
-        print(movie_summary)
-        print("--------------------------------------------")
+
+        # Replace print to write to file no,title,year,url,summary
+
+        #combine element in CSV as list
+        my_write.writerow([movie_title,movie_year,movie_summary])
+        # This will write to CSV but having empty line between rows
+
+            #myfile.write("%s\n" % movie_title)
+            #myfile.writelines("%s\n" % movie_year)
+
+        #print(movie_title, movie_year)
+        #print(movie_summary)
+        #print("--------------------------------------------")
+
+        print(ctr, " rows generated")
         ctr = ctr + 1
-        if (ctr == 10):
+        if (ctr == 100):
             break;
 
 
